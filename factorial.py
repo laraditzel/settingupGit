933bc37696e80cb8691995d405a97358bdb23e41
laraditzel <laraditzel@gmail.com>
2023-08-18 09:43:03 +0200

    factorial.py

--- factorial.py
+++ factorial.py
@@ -1,10 +1,9 @@
 def factorial(n):
     if n== 0:
         return 1
-    else
+    else:
         return n = factorial(n-1)
-
 number = 10
 result = factorial(number)
 print(f"the factorial of {number} is {result}.")
-
+print("additional line")
